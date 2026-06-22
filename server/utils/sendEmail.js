e8eaeab9d054d1098e6412820513932a450e9f76
kezar <kezar8234@gmail.com>
2026-06-22 23:02:15 +0530

    fix: Brevo SMTP for email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,24 +2,22 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, text, html) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,        
-    secure: true,     
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     }
   });
 
-  const mailOptions = {
+  await transporter.sendMail({
     from: `"SwapStyle" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
     html
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
 
 module.exports = sendEmail;
